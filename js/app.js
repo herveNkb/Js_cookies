9e2e714638ff5e42cfbfd9ea4693515cef1726f8
AirV <herve.jacotet@sfr.fr>
2022-11-16 08:15:16 +0100

    feat(cookie): created cookie Add function for create cookie with expiration date.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const inputs = document.querySelector("input");
+const inputs = document.querySelectorAll("input");
 
-// Écouteur événement "invalid" pour le message d'erreur si le champ est vide
+// Écouteur d'événement "invalid" pour le message d'erreur si le champ est vide
 // et l'écouteur "input" lorsque le champ n'est plus vide.
 inputs.forEach((input) => {
   input.addEventListener("invalid", handleValidation);
@@ -14,7 +14,47 @@ function handleValidation(event) {
       "Attention, veuillez mettre quelque chose dans ce champ"
     );
   } else if (event.type === "input") {
-    // Les guillemets vides de setCustomValidity permet de ne plus afficher le message d'erreur.
+    // Les guillemets vides de setCustomValidity permettent de ne plus afficher le message d'erreur.
     event.target.setCustomValidity("");
   }
 }
+
+
+// Création de l'objet "cookie"
+const cookieForm = document.querySelector("form");
+cookieForm.addEventListener("submit", handleForm);
+
+function handleForm(event) {
+  event.preventDefault()
+
+  const newCookie = {};
+
+  inputs.forEach(input => {
+    const nameAttribute = input.getAttribute("name")
+    newCookie[nameAttribute] = input.value;
+  })
+  // Date d'expiration du cookie (ici, c'est pour une 7jours)
+  newCookie.expires = new Date(new  Date().getTime() + 7 * 24 * 60 * 60 * 1000)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
